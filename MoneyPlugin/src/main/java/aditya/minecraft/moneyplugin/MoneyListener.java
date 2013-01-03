3a521520d374636112a9b9275d5f576d623b3718
Aditya Gupta <adityag@gmail.com>
2013-01-02 23:11:03 -0800

    Finally added shops! You can buy or sell items using signs. You will have to enter whether to buy or sell, the quantity, the block ID, and the price.

--- MoneyPlugin/src/main/java/aditya/minecraft/moneyplugin/MoneyListener.java
+++ MoneyPlugin/src/main/java/aditya/minecraft/moneyplugin/MoneyListener.java
@@ -4,12 +4,16 @@
 package aditya.minecraft.moneyplugin;
 
 import org.bukkit.ChatColor;
+import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockDamageEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.inventory.FurnaceExtractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 
 /**
  * @author Aditya Gupta
@@ -20,6 +24,10 @@ public class MoneyListener implements Listener {
      *
      */
     public double money = 0.00;
+    private boolean buy = false;
+    private int quantity = 0;
+    private int blockid = 0;
+    private double cost = 0.00;
 
     /**
      *
@@ -27,25 +35,69 @@ public class MoneyListener implements Listener {
      */
     @EventHandler
     public void blockBreak(BlockBreakEvent event) {
-        money = money + 0.1;
+        money = money + 0.01;
         Player player;
         player = event.getPlayer();
         player.sendMessage(ChatColor.AQUA + "You have $" + String.format("%1$.2f", money));
     }
-    
+
     @EventHandler
     public void blockPlace(BlockPlaceEvent event) {
-        money = money + 0.1;
+        money = money + 0.01;
         Player player;
         player = event.getPlayer();
         player.sendMessage(ChatColor.AQUA + "You have $" + String.format("%1$.2f", money));
     }
-    
+
     @EventHandler
     public void furnaceExtract(FurnaceExtractEvent event) {
-        money = money + 0.25;
+        money = money + 0.03;
         Player player;
         player = event.getPlayer();
         player.sendMessage(ChatColor.AQUA + "You have $" + String.format("%1$.2f", money));
+    }
+
+    @EventHandler
+    public void shopItem(BlockDamageEvent event) {
+        if (event.getBlock().getTypeId() == 68) {
+            Sign sign = (Sign) event.getBlock().getState();
+            if ("BUY".equals(sign.getLine(0))) {
+                buy = true;
+            }
+            if ("SELL".equals(sign.getLine(0))) {
+                buy = false;
+            }
+            quantity = Integer.valueOf(sign.getLine(1));
+            blockid = Integer.valueOf(sign.getLine(2));
+            cost = Double.valueOf(sign.getLine(3));
+
+            if (buy) {
+                if (money >= cost) {
+                    money -= cost;
+                    
+                    Player player;
+                    player = event.getPlayer();
+                    player.getInventory().addItem(new ItemStack(blockid, quantity));
+                    player.sendMessage(ChatColor.AQUA + "You have $" + String.format("%1$.2f", money));
+                }
+
+            }
+            if (money < cost) {
+                Player player;
+                player = event.getPlayer();
+                player.sendMessage(ChatColor.RED + "You do not have enough money! You have $" + String.format("%1$.2f", money));
+            }
+
+
+
+
+        }
+        if (!buy) {
+            money += cost;
+            Player player;
+            player = event.getPlayer();
+            player.getInventory().removeItem(new ItemStack(blockid, quantity));
+            player.sendMessage(ChatColor.AQUA + "You have $" + String.format("%1$.2f", money));
+        }
     }
 }
