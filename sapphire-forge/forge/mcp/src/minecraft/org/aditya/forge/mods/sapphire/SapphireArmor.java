343a81d5b74fd956c5e2c4afdf62ca2f59b4a29a
Aditya Gupta <adityag@gmail.com>
2014-02-20 21:20:14 -0800

    Changed the directory name so there is separate folders for each mod.

---
+++ sapphire-forge/forge/mcp/src/minecraft/org/aditya/forge/mods/sapphire/SapphireArmor.java
@@ -0,0 +1,51 @@
+package org.aditya.forge.mods.sapphire;
+
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
+import net.minecraft.item.EnumArmorMaterial;
+import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemStack;
+
+/**
+ * @author Aditya Gupta
+ */
+
+public class SapphireArmor extends ItemArmor {
+	public SapphireArmor(int par1, EnumArmorMaterial par2EnumArmorMaterial, int par3, int par4) {
+		super(par1, par2EnumArmorMaterial, par3, par4);
+		this.setCreativeTab(CreativeTabs.tabSapphires);
+	}
+
+	public String getArmorTexture(ItemStack stack, Entity entity, int slot, int layer) {
+		if (stack.itemID == SapphireMod.sapphirehelmet.itemID
+				|| stack.itemID == SapphireMod.sapphirechestplate.itemID
+				|| stack.itemID == SapphireMod.sapphireboots.itemID) {
+			return "SapphireMod:textures/armor/SapphireArmor_1.png";
+		}
+		if (stack.itemID == SapphireMod.sapphireleggings.itemID) {
+			return "SapphireMod:textures/armor/SapphireArmor_2.png";
+		} else {
+			return null;
+		}
+	}
+
+	public void registerIcons(IconRegister reg) { // Make sure to import
+													// IconRegister!
+		if (itemID == SapphireMod.sapphirechestplate.itemID) {
+			this.itemIcon = reg.registerIcon(SapphireMod.modid + ":" + (this.getUnlocalizedName().substring(5)));
+		}
+
+		if (itemID == SapphireMod.sapphireleggings.itemID) {
+			this.itemIcon = reg.registerIcon(SapphireMod.modid + ":" + (this.getUnlocalizedName().substring(5)));
+		}
+
+		if (itemID == SapphireMod.sapphireboots.itemID) {
+			this.itemIcon = reg.registerIcon(SapphireMod.modid + ":" + (this.getUnlocalizedName().substring(5)));
+		}
+
+		if (itemID == SapphireMod.sapphirehelmet.itemID) {
+			this.itemIcon = reg.registerIcon(SapphireMod.modid + ":" + (this.getUnlocalizedName().substring(5)));
+		}
+	}
+}
